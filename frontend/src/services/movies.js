10a15e28e28c8229c323fa8f71a2e8c76992e880
luvuyo-cyber <136644800+luvuyo-cyber@users.noreply.github.com>
2025-01-16 13:13:50 +0200

    Add axios dependency and implement MovieDataService for API interactions

---
+++ frontend/src/services/movies.js
@@ -0,0 +1,41 @@
+/*a service is a class with a well defined specific function your app needs
+-our service is responsible for talking to the backend to get and save data
+-they provide their functionality to be consumed by components */
+
+import axios from "axios";
+
+class MovieDataService {
+  getAll(page = 0) {
+    return axios.get(`http://localhost:4000/api/v1/movies?page=${page}`);
+  }
+
+  get(id) {
+    return axios.get(`http://localhost:4000/api/v1/movies/id/${id}`);
+  }
+
+  find(query, by = "title", page = 0) {
+    return axios.get(
+      `http://localhost:4000/api/v1/movies?${by}=${query}&page=${page}`
+    );
+  }
+
+  createReview(data) {
+    return axios.post("http://localhost:4000/api/v1/movies/review", data);
+  }
+
+  updateReview(data) {
+    return axios.put("http://localhost:4000/api/v1/movies/review", data);
+  }
+
+  deleteReview(id, userId) {
+    return axios.delete("http://localhost:4000/api/v1/movies/review", {
+      data: { review_id: id, user_id: userId },
+    });
+  }
+
+  getRatings() {
+    return axios.get("http://localhost:4000/api/v1/movies/ratings");
+  }
+}
+
+export default new MovieDataService();
