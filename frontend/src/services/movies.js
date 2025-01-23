f68f54eff6f1b12e5355a570f5f4eba7d2811830
luvuyo-cyber <136644800+luvuyo-cyber@users.noreply.github.com>
2025-01-23 17:39:34 +0200

    Update MovieDataService to use production API endpoints

--- frontend/src/services/movies.js
+++ frontend/src/services/movies.js
@@ -2,35 +2,50 @@ import axios from "axios";
 
 class MovieDataService {
   getAll(page = 0) {
-    return axios.get(`http://localhost:4000/api/v1/movies?page=${page}`);
+    return axios.get(
+      `https://mern-backend-uxk4.onrender.com/api/v1/movies?page=${page}`
+    );
   }
 
   get(id) {
-    return axios.get(`http://localhost:4000/api/v1/movies/id/${id}`);
+    return axios.get(
+      `https://mern-backend-uxk4.onrender.com/api/v1/movies/id/${id}`
+    );
   }
 
   find(query, by = "title", page = 0, rating) {
     return axios.get(
-      `http://localhost:4000/api/v1/movies?${by}=${query}&page=${page}`
+      `https://mern-backend-uxk4.onrender.com/api/v1/movies?${by}=${query}&page=${page}`
     );
   }
 
   createReview(data) {
-    return axios.post("http://localhost:4000/api/v1/movies/review", data);
+    return axios.post(
+      "https://mern-backend-uxk4.onrender.com/api/v1/movies/review",
+      data
+    );
   }
 
   updateReview(data) {
-    return axios.put("http://localhost:4000/api/v1/movies/review", data);
+    return axios.put(
+      "https://mern-backend-uxk4.onrender.com/api/v1/movies/review",
+      data
+    );
   }
 
   deleteReview(id, userId) {
-    return axios.delete("http://localhost:4000/api/v1/movies/review", {
-      data: { review_id: id, user_id: userId },
-    });
+    return axios.delete(
+      "https://mern-backend-uxk4.onrender.com/api/v1/movies/review",
+      {
+        data: { review_id: id, user_id: userId },
+      }
+    );
   }
 
   getRatings() {
-    return axios.get("http://localhost:4000/api/v1/movies/ratings");
+    return axios.get(
+      "https://mern-backend-uxk4.onrender.com/api/v1/movies/ratings"
+    );
   }
 }
 
